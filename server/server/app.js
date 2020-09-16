01cf5d958e305aa23db5df99df52c80c043ce6e8
makx <delmst@yandex.ru>
2020-09-16 21:44:55 +0300

    add mongoose models & updating resolve

--- server/server/app.js
+++ server/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
@@ -13,6 +14,21 @@ app.use(
   }),
 );
 
+async function start() {
+  try {
+    await mongoose
+      .connect(
+        'mongodb+srv://Maksim:pass123@cluster0.g0pcd.mongodb.net/graphql-tutorial?retryWrites=true&w=majority',
+        { useUnifiedTopology: true, useNewUrlParser: true },
+      )
+      .then(console.log('CONNECTED TO DB!'));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log(`Server started on port:${PORT}`);
 });
+
+start();
